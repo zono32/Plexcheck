49131b291af67bdf46ed6eee7edbfc0c4ee0e06a
kinso <masterzono32@gmail.com>
2024-12-23 12:51:22 +0100

    PC recuperar vista login anterior no guardaba en la store.

--- plexcheck/src/views/comodin.vue
+++ plexcheck/src/views/comodin.vue
@@ -1,37 +1,113 @@
 <script setup>
-import useService from "@/hooks/useService";
+import CryptoJS from "crypto-js";
 import { useAuthStore } from "@/stores/auth";
-import { useUserStore } from "@/stores/user";
-import { onMounted } from "vue";
+import useService from "@/hooks/useService";
 
-const auth = useAuthStore();
-const user = useUserStore();
+const encryptedLogin = (name, pass) =>
+  CryptoJS.AES.encrypt(
+    `"${JSON.stringify({
+      email: name,
+      password: pass,
+    }).replace(new RegExp('"', "g"), '\\"')}"`,
+    import.meta.env.VITE_CRYPTO_KEY
+  ).toString();
 
-const { 
-  getCurrentTimetable,
-  currentTimetableData
-} = useService();
+const userData = ref({ name: "", pass: "" });
+const switchState = ref(true);
 
-const dataTables = ref([]);
+const router = useRouter();
+const { authExecute, authData, isAuthSuccess } = useService();
 
-const ids = [8, 30];
-onMounted(async () => {
-  for (let i = 0; i < ids.length; i++) {
-  const id = ids[i];
-  console.log("id", id);
-  await getCurrentTimetable(user.companyId, id, auth.token);
-    if (currentTimetableData.value) {
-      dataTables.value.push({ ...currentTimetableData.value });
-      console.log(currentTimetableData.value);
-    }
-}  
-})
-console.log(dataTables.value);
+const encryptedData = computed(() =>
+  encryptedLogin(userData.value.name, userData.value.pass)
+);
+
+watch(isAuthSuccess, () => {
+  const auth = useAuthStore();
+  auth.setTokens(authData.value.data.token, switchState.value ? 36000 : 1200); 
+  auth.setUser(authData.value.data);
+  router.push("/"); 
+});
 
 </script>
 
-<template>  
-{{ dataTables }}
+<template>
+  <form class="content-form">
+    <div class="content-title">Iniciar sesión</div>
+
+    <AppInput
+      class="input-field-name"
+      v-model="userData.name"
+      label="Nombre"
+      placeholder="Introduzca su nombre de usuario"
+      type="text"
+    />
+    <AppInput
+      class="input-field-pass"
+      v-model="userData.pass"
+      label="Contraseña"
+      placeholder="Introduzca su contraseña"
+      type="password"
+    />
+    <AppSwitch
+      class="switch-field"
+      v-model="switchState"
+      label="Mantener la sesión iniciada"
+    />
+    <AppButton
+      class="submit-btn"
+      :disabled="!userData.name || !userData.pass"
+      title="Enviar"
+      @click="authExecute(encryptedData)"
+      style="background-color: #003356; color: white"
+    />
+  </form>
 </template>
 
-<style scoped></style>
+<style scoped>
+.content-form {
+  background-color: #f4f6f8;
+  padding: 2%;
+  width: 40%;
+  margin: auto;
+  margin-top: 50px;
+  border-radius: 0.5em;
+  box-shadow: 0 0.5em 1em rgba(0, 0, 0, 0.1);
+  font-family: "Arial", sans-serif;
+}
+
+.content-title {
+  font-size: 2.8em;
+  font-weight: bold;
+  color: #003356;
+  text-align: center;
+  margin-bottom: 50px;
+  border-bottom: 3px solid #d16c6d;
+}
+
+.switch-field {
+  margin-bottom: 1.5em;
+  display: flex;
+  align-items: center;
+  gap: 0.5em;
+  font-size: 1em;
+  color: #003356;
+}
+
+.submit-btn {
+  text-align: center;
+  margin-bottom: 30px;
+}
+
+@media (max-width: 768px) {
+  .content-form {
+    max-width: 90%;
+  }
+}
+
+@media (max-width: 480px) {
+  .content-form {
+    width: 95%;
+  }
+}
+</style>
