a56a16c611a18a3ff21c5a39c7bff4b13501946c
zono32 <147318166+zono32@users.noreply.github.com>
2025-01-23 12:10:09 +0100

    PC update

--- plexcheck/src/views/HomeView.vue
+++ plexcheck/src/views/HomeView.vue
@@ -493,8 +493,7 @@ const nextHoliday = computed(() => {
   background-color: #f9f9f9;
   padding: 15px;
   border-radius: 10px;
-  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
-  
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);  
 }
 
 .estimated_exit_hour {
